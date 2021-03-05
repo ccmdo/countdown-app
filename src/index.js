3bb839719bc210551b228aca8c65cec4fa65cfbe
Steven Hamilton <ccmdo90@gmail.com>
2021-03-05 12:25:49 +0000

    Remove unnecessary stuff

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import './main.css';
 import { Elm } from './Main.elm';
-import * as serviceWorker from './serviceWorker';
 
 var app = Elm.Main.init({
   node: document.getElementById('root')
 });
+
+
+// Subscriptions to Elm events
 
 app.ports.playMusic.subscribe(function(message) {
   let audioElement = document.querySelector('audio')
@@ -28,6 +29,9 @@ app.ports.stopMusic.subscribe(function(message) {
   audioElement.currentTime = 0;
 });
 
+
+// Ports to notify Elm
+
 document.querySelector('audio').addEventListener("play", function(event) {
   console.log("play");
   app.ports.audioStarted.send(null);
@@ -37,10 +41,3 @@ document.querySelector('audio').addEventListener("ended", function(event) {
   console.log("ended");
   app.ports.audioEnded.send(null);
 })
-
-app.ports.musicFinished
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
